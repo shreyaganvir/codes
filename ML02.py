cc81bdf7732d83210cf98baaee76560b17bd357e
shreyaganvir <shreyali94ganvir@gmail.com>
2024-03-13 15:24:18 +1300

    Update ML02.py

--- ML02.py
+++ ML02.py
@@ -6,6 +6,11 @@
 # Collect the Data
 
 import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_absolute_error
+
+
 bikes = pd.read_csv("bikes.csv")
 print(bikes.head())
 
@@ -13,10 +18,10 @@ print(bikes.head())
 
 print(bikes.info())
 print(bikes.describe())
-get_ipython().run_line_magic('matplotlib', 'inline')
-bikes.plot(kind = 'scatter', x = 'temperature', y = 'rentals')
-bikes.plot(kind = 'scatter', x = 'humidity', y = 'rentals')
-bikes.plot(kind = 'scatter', x = 'windspeed', y = 'rentals')
+#get_ipython().run_line_magic('matplotlib', 'inline')
+#bikes.plot(kind = 'scatter', x = 'temperature', y = 'rentals')
+#bikes.plot(kind = 'scatter', x = 'humidity', y = 'rentals')
+#bikes.plot(kind = 'scatter', x = 'windspeed', y = 'rentals')
 
 
 # Prepare the Data
@@ -26,24 +31,25 @@ predictors = list(bikes.columns)
 predictors.remove(response)
 x = bikes[predictors]
 
-from sklearn.model_selection import train_test_split
-x_train, x_test, y_train, y_test = train_test_split(x, y, random_state = 1234) 
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, random_state = 1234)
+model = LinearRegression().fit(x_train, y_train)
+print(model.intercept_)
+print(model.coef_)
 
 
 # Train the Model
-from sklearn.linear_model import LinearRegression
 
 # The model coefficients correspond to the order in which the independent variables are listed in the training data. This means that the equation for the fitted regression line can be written as:
-# 
 # $y = 3800.68 + 80.35 \times temperature - 4665.74 \times humidity - 196.22 \times windspeed$
-# 
 # With the linear regression equation, we can estimate what our model will predict given any weather condition. For example, given a temperature of $72^{\circ}F$, $22\%$ humidity and windspeed of $5$ miles per hour, our model would predict:
-# 
 # $7,578 \text{ bikes} \approx 3800.68 + 80.35 \times 72 - 4665.74 \times .22 - 196.22 \times 5$
-# 
 
 # Evaluate the Model
 
-from sklearn.metrics import mean_absolute_error
-print(mean_absolute_error(y_test, y_pred))
+model.score(x_test, y_test)
 
+
+y_pred = model.predict(x_test)
+
+print(mean_absolute_error(y_test, y_pred))
